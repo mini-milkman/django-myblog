76db89ac67346ffdc8706eaf9fc2b3da0e017422
mini-milkman <aihaozhe78@gmail.com>
2017-11-09 18:05:39 +0800

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # django-haystack
 HAYSTACK_CONNECTIONS = {
     'default': {
